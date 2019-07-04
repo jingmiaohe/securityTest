14a16baea3beef30a7f0ee15f977d0541c50cc79
hejm <13717536274@163.com>
2019-07-04 09:24:06 +0800

    题目字体变大一号

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -8,7 +8,8 @@ module.exports = {
     port: '8088',
     proxy: { // proxy URLs to backend development server
       '/': {
-        target: 'http://139.199.37.218:8080', // 设置你调用的接口域名和端口号
+        // target: 'http://139.199.37.218:8080', // 设置你调用的接口域名和端口号
+				target: 'http://bjkas.zayxy.com', // 设置你调用的接口域名和端口号
         changeOrigin: true,     // 跨域
         pathRewrite: {
           '^/': '/'
